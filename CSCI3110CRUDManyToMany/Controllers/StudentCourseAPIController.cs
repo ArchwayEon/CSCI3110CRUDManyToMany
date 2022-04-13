16850051a0cdb66d4a79983fbf340a19108be811
Jeff Roach <roachj@etsu.edu>
2022-04-13 15:33:02 -0400

    Create using WebAPI and AJAX

---
+++ CSCI3110CRUDManyToMany/Controllers/StudentCourseAPIController.cs
@@ -0,0 +1,35 @@
+﻿using CSCI3110CRUDManyToMany.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSCI3110CRUDManyToMany.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StudentCourseAPIController : ControllerBase
+{
+    private readonly IStudentRepository _studentRepo;
+    private readonly ICourseRepository _courseRepo;
+    private readonly IStudentCourseRepository _studentCourseRepo;
+
+    public StudentCourseAPIController(
+        IStudentRepository studentRepo,
+        ICourseRepository courseRepo,
+        IStudentCourseRepository studentCourseRepo)
+    {
+        _studentRepo = studentRepo;
+        _courseRepo = courseRepo;
+        _studentCourseRepo = studentCourseRepo;
+    }
+
+    [HttpPost("create")]
+    public IActionResult Post([FromForm] string ENumber, [FromForm]int courseId)
+    {
+        var studentCourseGrade = _studentCourseRepo.Create(ENumber, courseId);
+        studentCourseGrade?.Student?.CourseGrades.Clear();
+        studentCourseGrade?.Course?.StudentGrades.Clear();
+        return CreatedAtAction("Get", 
+            new { id = studentCourseGrade?.Id }, studentCourseGrade);
+    }
+
+}
---
+++ CSCI3110CRUDManyToMany/wwwroot/js/studentCourseCreate.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+(function _studentCourseGrade() {
+    const formCreateStudentCourse =
+        document.querySelector("#formCreateStudentCourse");
+    formCreateStudentCourse.addEventListener('submit', e => {
+        e.preventDefault();
+        const url = "/api/studentcourseapi/create";
+        const method = "post";
+        const formData = new FormData(formCreateStudentCourse);
+        console.log(`${url} ${method}`);
+
+        fetch(url, {
+            method: method,
+            body: formData
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('There was a network error!');
+            }
+            return response.json();
+        })
+        .then(result => {
+            console.log('Success: the student course record was created');
+            window.location.replace(`/student/details/${result.studentENumber}`); // redirect
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+
+})();
